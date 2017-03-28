90f8e46748ef5885562feb213e83d81712203093
Balabusko Viktor <pravosudor@gmail.com>
2017-03-28 12:04:50 +0400

    sggshshgsg

--- src/by/htp/task3/Test.java
+++ src/by/htp/task3/Test.java
@@ -8,12 +8,12 @@ public class Test {
 	poezd.sostavWagons[1].setPasangers(100);
 	poezd.sostavWagons[2].setPasangers(20);
 	poezd.sostavWagons[3].setPasangers(25);
-	//poezd.sostavWagons[4].setPasangers(30);
+	poezd.sostavWagons[4].setPasangers(30);
 	poezd.sostavWagons[5].setPasangers(35);
 	poezd.sostavWagons[6].setPasangers(40);
 	poezd.sostavWagons[7].setPasangers(15);
 	poezd.sostavWagons[9].setKomfort(1);
-	//poezd.sostavWagons[7].setKomfort(2);
+	poezd.sostavWagons[7].setKomfort(2);
 	poezd.sostavWagons[5].setKomfort(3);
 	poezd.sostavWagons[8].setKomfort(1);
 	poezd.sostavWagons[1].setKomfort(2);
